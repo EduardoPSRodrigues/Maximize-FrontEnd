668fa9a6a7d8d9f57dd9dc76c9bcdacb9a748a1f
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-28 17:47:56 -0300

    Criação da rota para o blog

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
+import ListNews from '../views/ListNews.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/listnews',
+      name: 'ListNews',
+      component: ListNews
     }
   
   ]
